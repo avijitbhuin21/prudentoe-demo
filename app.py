a4346127a1f357cc653c2685e502af695c143a5f
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-06-01 02:05:46 +0530

    updated some ui elements

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return render_template('trial.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
