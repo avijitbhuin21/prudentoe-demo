4911771fbc51bca2d3670422152b5f0ec767e7b3
avijitbhuin21 <avijitbhuin21@gmail.com>
2025-06-01 17:22:47 +0530

    final ui update

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('trial.html')
+# @app.route('/test')
+# def test():
+#     return render_template('trial.html')
 
-@app.route('/fix')
-def fix():
-    return render_template('fix.html')
+# @app.route('/fix')
+# def fix():
+#     return render_template('fix.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
